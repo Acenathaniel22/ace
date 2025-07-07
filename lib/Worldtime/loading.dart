beaff9081e55fba76cdebf7b7daa44f57b17a93f
Acenathaniel22 <sinagacenathaniel@gmail.com>
2025-07-07 10:50:19 +0800

    commit

---
+++ lib/Worldtime/loading.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class Loading extends StatefulWidget {
+  const Loading({super.key});
+
+  @override
+  State<Loading> createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+  void getData() async {
+    http.Response response = await http.get(
+      Uri.parse('https://jsonplaceholder.typicode.com/todos/1'),
+    );
+    Map data = jsonDecode(response.body);
+    print(data);
+    print(data['title']);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Text('loading screen'));
+  }
+}
